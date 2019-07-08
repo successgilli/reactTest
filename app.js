bb23c3df2426112cd978bb4c1aae363f310bc9f4
Gilbert Awaji-Mitop <gilbertawaji-mitop@Gilberts-MacBook-Pro.local>
2019-07-08 16:48:51 +0100

    removed int from port and added .js to start script

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const path = require('path')
 const app = express();
-const port = parseInt(process.env.PORT,10) || 3000;
+const port = process.env.PORT || 3000;
 app.use(express.static(path.resolve(__dirname, './public')));
 app.get('*', (req, res) => {
     res.sendFile(path.resolve(__dirname, './public/index.html'));
