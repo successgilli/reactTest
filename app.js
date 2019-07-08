cd4f4db52ac4047ccea74399891be647b67c160d
Gilbert Awaji-Mitop <gilbertawaji-mitop@Gilberts-MacBook-Pro.local>
2019-07-08 16:55:25 +0100

    removed es6 completely from app.js

--- app.js
+++ app.js
@@ -1,11 +1,10 @@
-const express = require('express');
-const path = require('path')
-const app = express();
-const port = process.env.PORT || 3000;
+var express = require('express');
+var path = require('path');
+var app = express();
 app.use(express.static(path.resolve(__dirname, './public')));
 app.get('*', (req, res) => {
     res.sendFile(path.resolve(__dirname, './public/index.html'));
 })
-app.listen(port, () => {
-    console.log(`listening on port ${port}`)
+app.listen(process.env.PORT || 3000, () => {
+    console.log(`listening on port ${3000}`);
 })
