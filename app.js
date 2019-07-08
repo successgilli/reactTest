833194e6c061071c17c74ac98f680df44387fd30
Gilbert Awaji-Mitop <gilbertawaji-mitop@Gilberts-MacBook-Pro.local>
2019-07-08 15:44:53 +0100

    changed port to PORT

--- app.js
+++ app.js
@@ -6,6 +6,6 @@ app.use(express.static('public'))
 app.get('*', (req, res) => {
     res.sendFile(path.join(__dirname, '../public/index.html'));
 })
-app.listen(process.env.Port || port, () => {
+app.listen(process.env.PORT || port, () => {
     console.log(`listening on port ${port}`)
 })
