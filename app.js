0fb9dd0906336f18faa1b9dd0db7eb74add6dcfc
Gilbert Awaji-Mitop <gilbertawaji-mitop@Gilberts-MacBook-Pro.local>
2019-07-08 16:42:15 +0100

    used require instaed of import in app.js

--- app.js
+++ app.js
@@ -1,10 +1,10 @@
-import express from 'express';
-import path from 'path'
+const express = require('express');
+const path = require('path')
 const app = express();
-const port = process.env.PORT || 3000;
-app.use(express.static(path.resolve(__dirname, '../public')));
+const port = parseInt(process.env.PORT,10) || 3000;
+app.use(express.static(path.resolve(__dirname, './public')));
 app.get('*', (req, res) => {
-    res.sendFile(path.resolve(__dirname, './index.html'));
+    res.sendFile(path.resolve(__dirname, './public/index.html'));
 })
 app.listen(port, () => {
     console.log(`listening on port ${port}`)
